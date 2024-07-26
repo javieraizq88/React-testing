dc6270306990068c90c347e3ff49536be6193880
javiera izquierdo <jizquierdo.3it@bancochile.cl>
2024-07-26 10:55:33 -0400

    test mostrar 6 productos, click en load more y mostrar 6  productos mas

--- src/App.test.js
+++ src/App.test.js
@@ -2,14 +2,14 @@ import { render, screen, waitFor } from '@testing-library/react';
 import user from '@testing-library/user-event';
 import App from './App';
 
-test('shows 6 products by default', async () => {
+test('mostrat 6 productos por default', async () => {
   render(<App />);
 
   const titles = await screen.findAllByRole('heading');
   expect(titles).toHaveLength(6);
 });
 
-test('clicking on the buton loads 6 more products', async () => {
+test('click en boton load more y mostrar 6 productos más en pantalla', async () => {
   render(<App />);
   const button = await screen.findByRole('button', {
     name: /load more/i,
